ad333764f0e7f8f310b5854eea3e35e8f5bae680
tomato_rain <35755025+shagody@users.noreply.github.com>
2018-05-04 11:18:12 +0800

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+			ArrayList<String> a = new ArrayList<String>();
+			
+			while(scan.hasNext()){
+				a.add(scan.nextLine());
+			}
+			int c[]=new int[100];
+			for(int i=0;i<a.size();i++){
+				c[i]=Integer.parseInt(a.get(i).substring(7, 12));
+			}
+			
+			int cc[]=new int[100];
+			cc[0]=0;
+			for(int i=0;i<a.size();i++){
+				for(int j=0;j<a.size();j++){
+					if(c[j]>c[j+1]&&c[j]>c[cc[i]])
+						cc[j]=j+1;
+				}
+			}
+			for(int i=0;i<a.size();i++){
+				System.out.println(a.get(cc[i]));
+			}
+
+		
+		scan.close();
+
+	}
+}
