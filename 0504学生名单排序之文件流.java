79ef2142e851a7af22697bdd513b1dcb33b45ae5
tomato_rain <35755025+shagody@users.noreply.github.com>
2018-05-05 10:35:53 +0800

    Update 0504学生名单排序之文件流.java

--- 0504学生名单排序之文件流.java
+++ 0504学生名单排序之文件流.java
@@ -3,58 +3,62 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Main {
+	public void bubble_sort(ArrayList<String> num, ArrayList<String> name, int lenth) {//冒泡排序
+		String temp0;
+		String temp1;
+		for (int i = 0; i < lenth - 1; i++) {
+			for (int j = 0; j < lenth - i - 1; j++) {
+				if (num.get(j).compareTo(num.get(j + 1)) >= 0) {
+					temp0 = num.get(j);
+					num.set(j, num.get(j + 1));
+					num.set(j + 1, temp0);
+					temp1 = name.get(j);
+					name.set(j, name.get(j + 1));
+					name.set(j + 1, temp1);
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 
 		try {
-			String sortFile="班级名单-sorted.txt";
-			DataOutputStream output = 
-					new DataOutputStream(new FileOutputStream(sortFile));
-			
+			String sortFile = "班级名单-sorted.txt";
+			DataOutputStream output = new DataOutputStream(new FileOutputStream(sortFile));
+
 			String strFile = "./班级名单.txt";
-	         //打开文件
-		    BufferedReader readfile= 
-		    		 new BufferedReader(new InputStreamReader(new FileInputStream(strFile)));
-			
+			// 打开文件
+			BufferedReader readfile = new BufferedReader(new InputStreamReader(new FileInputStream(strFile)));
+
 			ArrayList<String> num = new ArrayList<String>();
 			ArrayList<String> name = new ArrayList<String>();
-			
+
 			String b;
 			while ((b = readfile.readLine()) != null) {
-				String split[]=b.split("\t", 2);
+				String split[] = b.split("\t", 2);
 				num.add(split[0]);
 				name.add(split[1]);
 			}
-
-			String temp0;
-			String temp1;
+			// 冒泡排序
 			int lenth = num.size();
-			for (int i = 0; i < lenth - 1; i++) {
-				for (int j = 0; j < lenth - i - 1; j++) {
-					if (num.get(j).compareTo(num.get(j + 1)) >= 0) {
-						temp0 = num.get(j);
-						num.set(j, num.get(j + 1));
-						num.set(j + 1, temp0);
+			Main test = new Main();
+			test.bubble_sort(num, name, lenth);
 
-						temp1 = name.get(j);
-						name.set(j, name.get(j + 1));
-						name.set(j + 1, temp1);
-					}
-				}
-			}
+			// 输出排序好的名单
 			for (int i = 0; i < lenth; i++) {
 				output.writeUTF(num.get(i) + " " + name.get(i));
 			}
-			
+
+			// 顺序查找
 			Scanner scan = new Scanner(System.in);
 			while (scan.hasNext()) {
-				String no=scan.next();
-				for(int j=0;j<lenth;j++) {
-					if(no.compareTo(num.get(j))==0)
+				String no = scan.next();
+				for (int j = 0; j < lenth; j++) {
+					if (no.compareTo(num.get(j)) == 0)
 						System.out.println(name.get(j));
 				}
 			}
-			
+
 			output.close();
 			scan.close();
 			readfile.close();
