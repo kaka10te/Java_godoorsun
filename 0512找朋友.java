0d765e1c31e54d9adb460bbe8fb0e5554110103b
tomato_rain <35755025+shagody@users.noreply.github.com>
2018-05-12 23:21:32 +0800

    Update 0512找朋友.java

--- 0512找朋友.java
+++ 0512找朋友.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -5,25 +7,23 @@ import java.util.TreeMap;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		while (scan.hasNext()) {
 			int n = scan.nextInt();
-			TreeMap<String,String> chlid=new TreeMap<String,String>();
+			ArrayList<String> chlid=new ArrayList<String>();
 			TreeMap<Long,String> num=new TreeMap<Long,String>();
 			for(int i=0;i<n;i++) {
-				chlid.put(scan.next(), "0");
+				chlid.add(scan.next());
 			}
+			Collections.sort(chlid);//排序
 			for(int i=0;i<n;i++) {
 				num.put(scan.nextLong(), "0");
 			}
-			Iterator<String> iter1 = chlid.keySet().iterator();
+			int len=chlid.size();
 			Iterator<Long> iter2 = num.keySet().iterator();
-			while (iter1.hasNext()) {
-				Object ch = iter1.next();
+			for(int i=0;i<len;i++) {
 				Object no = iter2.next();
-//				chlid.put(chlid.get(ch), no.toString());
-				System.out.println(ch.toString()+' '+no.toString());
+				System.out.println(chlid.get(i)+' '+no.toString());
 			}
 		}
 		scan.close();
